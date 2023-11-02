0fe75fc88f16fc32a591d7d7128774f868a62758
Genaro Peque, Jr <gpequejr@gmail.com>
2023-11-02 22:27:45 +0900

    Create check_enabled_commands.php
    List all enabled PHP built-in commands

---
+++ check_enabled_commands.php
@@ -0,0 +1,7 @@
+<?php
+
+// List all enabled PHP built-in commands
+print_r(preg_grep("/^(system|exec|shell_exec|passthru|proc_open|popen|curl_exec|curl_multi_exec|parse_ini_file|show_source)$/", get_defined_functions(TRUE)["internal"]));
+
+
+?>
